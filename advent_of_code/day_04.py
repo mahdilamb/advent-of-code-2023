f1514d860d427c1d9fb690c23f3a630f0729dca4
Mahdi Lamb <mahdilamb@gmail.com>
2023-12-05 23:12:53 +0000

    Day 4 part 2

--- advent_of_code/day_04.py
+++ advent_of_code/day_04.py
@@ -1,4 +1,6 @@
+import functools
 import re
+from typing import Callable
 
 from advent_of_code import utils
 
@@ -7,7 +9,9 @@ CARD_WINNING_MINE = re.compile(r"Card\s*(\d+):\s*(.*?)\s*\|\s*(.*)")
 DIGITS = re.compile(r"\d+")
 
 
-def scores(input: str) -> dict[int, int]:
+def scores(
+    input: str, calc_score: Callable[[int], int] = lambda x: 2 ** (x - 1)
+) -> dict[int, int]:
     """Calculate the core for each scratch card."""
     results = {}
     for match in CARD_WINNING_MINE.finditer(input):
@@ -16,16 +20,34 @@ def scores(input: str) -> dict[int, int]:
         mine = set(DIGITS.findall(_mine))
         matches = mine & winning
         if len(matches):
-            results[int(card)] = 2 ** (len(matches) - 1)
+            results[int(card)] = calc_score(len(matches))
         else:
             results[int(card)] = 0
     return results
 
 
+def num_cards(input: str) -> int:
+    """Count the number of cards."""
+    card_scores = scores(input, calc_score=lambda x: x)
+    cards = sorted(card_scores)
+    winnings = []
+
+    def count_cards(card: int) -> None:
+        score = card_scores[card]
+        last_card = min(card + score, cards[-1])
+        winnings.extend(cards[card:last_card])
+        for c in cards[card:last_card]:
+            count_cards(c)
+
+    for card in cards:
+        count_cards(card)
+    return len(winnings) + len(cards)
+
+
 def main():
     with utils.contents() as contents:
-        print(scores(contents))
         print("Part one:", sum(scores(contents).values()))
+        print("Part two:", num_cards(contents))
 
 
 if __name__ == "__main__":
